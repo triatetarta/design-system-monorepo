1c70aaaf89b32235588254b1e2963f91449d2915
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2023-01-22 12:36:11 +0000

    initial setup and boilerplate

---
+++ packages/ui/dist/index.mjs
@@ -0,0 +1,8 @@
+// src/components/Button/Button.tsx
+import { jsx } from "react/jsx-runtime";
+var Button = () => {
+  return /* @__PURE__ */ jsx("button", { className: "rounded-md bg-blue-500 px-4 py-2 text-white", children: "Boop" });
+};
+export {
+  Button
+};
---
+++ packages/ui/src/components/Button/Button.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+export const Button = () => {
+  return (
+    <button className='rounded-md bg-blue-500 px-4 py-2 text-white'>
+      Boop
+    </button>
+  );
+};
